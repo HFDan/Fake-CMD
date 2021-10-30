f5c04177ba34f3b2f9503312b7dbdad61f521b2a
HFDan <58553089+HFDan@users.noreply.github.com>
2021-10-30 23:39:37 +0300

    Fixed a bug where the "Scanning file: " message would not display at the right time, but after the loop would finish

--- src/modules/tree.cpp
+++ src/modules/tree.cpp
@@ -11,7 +11,7 @@ const char* CLEAR_COMMAND = "clear";
 #endif
 
 MODULEFUNC(tree) {
-	// TODO: This function seems to not work, atleast under linux
+	
 	std::random_device randomDevice;
 	std::mt19937 rng(randomDevice());
 	std::uniform_int_distribution<std::mt19937::result_type> distribution(100, 500);
@@ -20,12 +20,13 @@ MODULEFUNC(tree) {
 
 		system(CLEAR_COMMAND);
 		printf("Scanning file %llu", i);
+		fflush(stdout);
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
 	}
 
 	system(CLEAR_COMMAND);
-	printf(u8"\nAll files scanned:\n\n|___________|___________|______________|__________________________|\n|  Viruses  |  Hackers  | IP Infection |        Conclusion        |\n|___________+___________+______________+__________________________|\n| 0         | 0         | None         | PC: Healthy              |\n| All Clear | All Clear | All Clear    | Tech Support: Not needed |\n|           |           |              |                          |\n|___________|___________|______________|__________________________|\n\n\n");
+	printf("\nAll files scanned:\n\n|___________|___________|______________|__________________________|\n|  Viruses  |  Hackers  | IP Infection |        Conclusion        |\n|___________+___________+______________+__________________________|\n| 0         | 0         | None         | PC: Healthy              |\n| All Clear | All Clear | All Clear    | Tech Support: Not needed |\n|           |           |              |                          |\n|___________|___________|______________|__________________________|\n\n\n");
 	std::this_thread::sleep_for(std::chrono::seconds(10));
 	system(CLEAR_COMMAND);
 
