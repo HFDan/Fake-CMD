5cd32c59b5cc04a8685648c4e9fa0c211642c727
HFDan <58553089+HFDan@users.noreply.github.com>
2021-10-31 11:56:30 +0200

    Bugfix inside syskey module, and updated the monolithic test to pass a debug argument.

--- src/modules/syskey.cpp
+++ src/modules/syskey.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <cstdio>
+#include <iterator>
 #ifdef WIN32
 #include <windows.h>
 #endif
@@ -7,12 +9,14 @@
 #ifndef WIN32
 MODULEFUNC(syskey) {
 	fputs("This module cannot run on macos/Linux, Sorry...\n", stderr);
-
 	return 0;
 }
 #else
 MODULEFUNC(syskey) {
-	MessageBox(NULL, "Syskey has encountered a problem and must quit.\nError code: B3nch0d", "Syskey", MB_OK | MB_ICONERROR);
+	
+	if (std::find(std::begin(argv), std::end(argv), "debug") == std::end(argv)) {
+		MessageBox(NULL, "Syskey has encountered a problem and must quit.\nError code: B3nch0d", "Syskey", MB_OK | MB_ICONERROR);	
+	}
 
 	return 0;
 }
--- test/monolithicTest.cpp
+++ test/monolithicTest.cpp
@@ -19,7 +19,7 @@ TEST(MonolithicTest, ArgumentParser) {
 
 TEST(MonolithicTest, AllModules) {
 	for (const auto &it : modules) {
-		std::vector<std::string> args = {it.first};
+		std::vector<std::string> args = {it.first, "debug"};
 		ASSERT_NE(it.second, nullptr) << "Command: " << it.first << " returns nullptr. Aborting...";
 		EXPECT_EQ(0, it.second(args));
 	}
