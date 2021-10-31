9c78515d24c6b80a1fa6a4b0b658d0964184991d
HFDan <58553089+HFDan@users.noreply.github.com>
2021-10-31 22:02:25 +0200

    Swiftly fixes unit tests

--- test/monolithicTest.cpp
+++ test/monolithicTest.cpp
@@ -3,19 +3,8 @@
 #include <string>
 #include <vector>
 #include "modules.h"
-#include "utility/utility.h"
 
 extern std::map<std::string, module> modules;
-
-TEST(MonolithicTest, ArgumentParser) {
-	std::string withArgs = "command -s -t -i -a -l";
-	std::vector<std::string> withArgsV = {"command", "-s", "-t", "-i", "-a", "-l"};
-	ASSERT_EQ(withArgsV, tokenizeCommand(withArgs));
-
-	std::string withoutArgs = "command";
-	std::vector<std::string> withoutArgsV = {"command"};
-	ASSERT_EQ(withoutArgsV, tokenizeCommand(withoutArgs));
-}
 
 TEST(MonolithicTest, AllModules) {
 	for (const auto &it : modules) {
